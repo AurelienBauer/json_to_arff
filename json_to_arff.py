cecd1a75bdd06af2b15c932d6394e42191beaa4a
aurelien.bauer <aurelien.aurelien.bauer@epitech.eu>
2019-07-18 10:30:33 +0200

    script json_to_arff
    ended at 90%

---
+++ json_to_arff.py
@@ -0,0 +1,194 @@
+#!/usr/bin/python3
+import sys, json, os, ntpath
+
+attributes = {
+    "a": {
+        "name": "primaryCode",
+        "type": "NUMERIC"
+    },
+    "b": {
+        "name": "XOnPress",
+        "type": "NUMERIC"
+    },
+    "c": {
+        "name": "XOnRelease",
+        "type": "NUMERIC"
+    },
+    "d": {
+        "name": "YOnPress",
+        "type": "NUMERIC"
+    },
+    "e": {
+        "name": "YOnRelease",
+        "type": "NUMERIC"
+    },
+    "f": {
+        "name": "NoKeyPressDelay",
+        "type": "NUMERIC"
+    },
+    "g": {
+        "name": "KeyPressDelay",
+        "type": "NUMERIC"
+    },
+    "i": {
+        "name": "PressureOnPress",
+        "type": "NUMERIC"
+    },
+    "j": {
+        "name": "PressureOnRelease",
+        "type": "NUMERIC"
+    },
+    "k": {
+        "name": "vectorCoord",
+        "type": "STRING"
+    },
+    "l": {
+        "name": "RotationVectorOnPress",
+        "type": "NUMERIC"
+    },
+    "m": {
+        "name": "RotationVectorOnRelease",
+        "type": "NUMERIC"
+    },
+    "n": {
+        "name": "LinearAccelerationOnPress",
+        "type": "NUMERIC"
+    },
+    "o": {
+        "name": "LinearAccelerationOnRelease",
+        "type": "NUMERIC"
+    },
+    "p": {
+        "name": "keyLabel",
+        "type": "STRING"
+    },
+}
+
+
+def print_help():
+    print("HELP")
+    for attr in attributes:
+        print(attr + " = " + attributes[attr])
+
+
+def parse_args():
+    params = ""
+    in_file = ""
+    out_file = ""
+
+    for arg in sys.argv[1:]:
+        if arg[0] == "-":
+            params += arg[1:]
+        elif len(in_file) < 1:
+            in_file = arg
+        elif len(out_file) < 1:
+            out_file = arg
+        else:
+            print_help()
+            exit(-1)
+
+    if len(in_file) < 1 or len(out_file) < 1:
+        print_help()
+        exit(-1)
+    return params, in_file, out_file
+
+
+def open_parse_file(in_file):
+    try:
+        file = open(in_file, 'r')
+        _json = json.loads(file.read())
+        return _json, path_leaf(file.name)
+    except IOError:
+        print("Error: can\'t find file or read data")
+        exit(-1)
+    except ValueError:
+        print("An error occur during the file is convert in json")
+        exit(-1)
+    except Exception as e:
+        print (e)
+        print("Unexpected error :/")
+        exit(-1)
+
+
+def path_leaf(path):
+    head, tail = ntpath.split(path)
+    return tail or ntpath.basename(head)
+
+
+def create_output_file(out_file, in_file_name):
+    try:
+        if os.path.isdir(out_file):
+            if out_file[-1:] != "/":
+                out_file += '/'
+            out_file += in_file_name + ".arff"
+
+        if os.path.exists(out_file) and os.path.isfile(out_file):
+            print("File already exist and it was removed.")
+            os.remove(out_file)
+
+        fd = open(out_file, 'w+')
+        fd.write(   "% Generate by Json_to_arff script\n" +
+                    "% Author: Aurélien Bauer\n" +
+                    "% For an academic project at the University of Kent\n"+
+                    "@RELATION continuous_authentication\n\n")
+        return fd
+    except IOError as e:
+        print(e)
+        exit(-1)
+
+
+def format_hard_sensors_data(letter, key):
+    line = ""
+    for coord in "xyz":
+        i = 0
+        while i < 5:
+            line += json.dumps(key[attributes[letter]['name']][coord][i]) + ","
+            i += 1
+    return line
+
+
+def format_hard_sensors_attributes(letter):
+    line = ""
+    for coord in "XYZ":
+        i = 0
+        while i < 5:
+            line += "@ATTRIBUTE " + attributes[letter]['name'] + coord + str(i) + ' ' + attributes[letter]['type'] + "\n"
+            i += 1
+
+    return line
+
+
+def write_attributes_data(fd, json_data, params):
+    for letter in params:
+        if letter in "lmno":
+            line = format_hard_sensors_attributes(letter)
+        else:
+            line = "@ATTRIBUTE " + attributes[letter]['name'] + " " + attributes[letter]['type'] + "\n"
+        fd.write(line)
+
+    fd.write("\n@DATA\n")
+
+    for section in json_data:
+        for key in json_data[section]:
+            line = ""
+            for letter in params:
+                if letter in "lmno":
+                    line += format_hard_sensors_data(letter, key)
+                else:
+                    line += json.dumps(key[attributes[letter]['name']]) + ","
+            fd.write(line[:-1] + '\n')
+
+def main():
+    params, in_file, out_file = parse_args()
+    json_data, in_file_name = open_parse_file(in_file)
+    out_file_fd = create_output_file(out_file, in_file_name)
+    write_attributes_data(out_file_fd, json_data, params)
+
+    print(params)
+    print(in_file)
+    print(out_file)
+    # print(json.dumps(json_data))
+
+
+if __name__ == "__main__":
+   main()
