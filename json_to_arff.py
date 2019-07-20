5e05c1ab92f43c52f81393c5f78dc7e32d40c6c3
aurelien.bauer <aurelien.aurelien.bauer@epitech.eu>
2019-07-20 11:27:54 +0200

    script json_to_arff
    ended at 100
    %

--- json_to_arff.py
+++ json_to_arff.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 import sys, json, os, ntpath
 
+PARAMS_LIST = "abcdefgijklmnop"
+
 attributes = {
     "a": {
         "name": "primaryCode",
@@ -66,9 +68,10 @@ attributes = {
 
 
 def print_help():
-    print("HELP")
+    print(" usage: json_to_arff.py [-abcdefgijklmnop] <input file path> <output file path>\n"
+          " options [lmno] could be following by a number between 0 and 5, if no number are choosen the default value is 5.\n")
     for attr in attributes:
-        print(attr + " = " + attributes[attr])
+        print(attr + " = " + attributes[attr]['name'])
 
 
 def parse_args():
@@ -78,6 +81,9 @@ def parse_args():
 
     for arg in sys.argv[1:]:
         if arg[0] == "-":
+            if len(arg) > 1 and arg[1] == 'h':
+                print_help()
+                exit(0)
             params += arg[1:]
         elif len(in_file) < 1:
             in_file = arg
@@ -90,6 +96,9 @@ def parse_args():
     if len(in_file) < 1 or len(out_file) < 1:
         print_help()
         exit(-1)
+
+    if len(params) < 1:
+        params = PARAMS_LIST
     return params, in_file, out_file
 
 
@@ -137,21 +146,31 @@ def create_output_file(out_file, in_file_name):
         exit(-1)
 
 
-def format_hard_sensors_data(letter, key):
+def parse_number_in_params(next_letter):
+    if next_letter.isdigit():
+        nbr = int(next_letter)
+        return 5 if nbr > 5 else nbr
+    return 5
+
+def format_hard_sensors_data(letter, next_letter, key):
     line = ""
+    j = parse_number_in_params(next_letter)
+
     for coord in "xyz":
         i = 0
-        while i < 5:
+        while i < j:
             line += json.dumps(key[attributes[letter]['name']][coord][i]) + ","
             i += 1
     return line
 
 
-def format_hard_sensors_attributes(letter):
+def format_hard_sensors_attributes(letter, next_letter):
     line = ""
+    j = parse_number_in_params(next_letter)
+
     for coord in "XYZ":
         i = 0
-        while i < 5:
+        while i < j:
             line += "@ATTRIBUTE " + attributes[letter]['name'] + coord + str(i) + ' ' + attributes[letter]['type'] + "\n"
             i += 1
 
@@ -159,35 +178,40 @@ def format_hard_sensors_attributes(letter):
 
 
 def write_attributes_data(fd, json_data, params):
+    i = 0
     for letter in params:
-        if letter in "lmno":
-            line = format_hard_sensors_attributes(letter)
-        else:
-            line = "@ATTRIBUTE " + attributes[letter]['name'] + " " + attributes[letter]['type'] + "\n"
-        fd.write(line)
+        if letter in PARAMS_LIST:
+            if letter in "lmno":
+                next_letter = "" if len(params) <= (i+1) else params[i+1]
+                line = format_hard_sensors_attributes(letter, next_letter)
+            else:
+                line = "@ATTRIBUTE " + attributes[letter]['name'] + " " + attributes[letter]['type'] + "\n"
+            fd.write(line)
+        i += 1
 
     fd.write("\n@DATA\n")
 
     for section in json_data:
         for key in json_data[section]:
             line = ""
+            i = 0
             for letter in params:
-                if letter in "lmno":
-                    line += format_hard_sensors_data(letter, key)
-                else:
-                    line += json.dumps(key[attributes[letter]['name']]) + ","
+                if letter in PARAMS_LIST:
+                    if letter in "lmno":
+                        next_letter = "" if len(params) <= (i+1) else params[i + 1]
+                        line += format_hard_sensors_data(letter, next_letter, key)
+                    else:
+                        line += json.dumps(key[attributes[letter]['name']]) + ","
+                i += 1
             fd.write(line[:-1] + '\n')
+
 
 def main():
     params, in_file, out_file = parse_args()
     json_data, in_file_name = open_parse_file(in_file)
     out_file_fd = create_output_file(out_file, in_file_name)
     write_attributes_data(out_file_fd, json_data, params)
-
-    print(params)
-    print(in_file)
-    print(out_file)
-    # print(json.dumps(json_data))
+    print("Arff file has been generated: " + out_file_fd.name)
 
 
 if __name__ == "__main__":
